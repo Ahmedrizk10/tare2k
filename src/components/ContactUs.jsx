0ba9eba39f80b0edc569a8fdc96f253aa811f611
ZeyadAbdallahSayed <zeyadabdallah01@gmail.com>
2025-09-06 20:34:09 +0300

    updates

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import { IoLogoFacebook } from "react-icons/io";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaMapPin } from "react-icons/fa";
+import ScrollToTop from './ScrollToTop'
 
-const ContactUs = () => {
+
+
+const Contact = () => {
   return (
+    <>
+<div className="min-h-screen dark:bg-[#020618] flex items-center justify-center py-16 px-6 ">    
+  <div className="grid md:grid-cols-2 gap-10 w-full max-w-6xl bg-white/2 backdrop-blur-xl p-10 rounded-2xl shadow-2xl">
+    <ScrollToTop />
     <div>
-      <h1>اتصل بنا</h1>
+      <h2 className="text-4xl font-bold  mb-6 text-[#070B1D] dark:text-white">كن علي تواصل معنا دائما </h2>
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder="اسمك "
+          className="p-4 rounded-xl bg-[#464e6e36] text-black dark:text-white outline-none border-none focus:outline-[#070B1D] focus:border-[#070B1D] " />
+        <input type="email" placeholder="بريدك الالكتروي "
+          className="p-4 rounded-xl bg-[#464e6e36] text-black dark:text-white outline-none border-none focus:outline-[#070B1D] focus:border-[#070B1D] " />
+        <input type="text" placeholder="الموضوع"
+          className="p-4 rounded-xl bg-[#464e6e36] text-black dark:text-white outline-none border-none focus:outline-[#070B1D] focus:border-[#070B1D] " />
+        <textarea placeholder="رسالتك ..." rows="5"
+          className="p-4 rounded-xl bg-[#464e6e36] text-black dark:text-white outline-none border-none focus:outline-[#070B1D] focus:border-[#070B1D] "></textarea>
+        <button className="mt-4 py-3 bg-[#13182d] font-bold text-lg rounded-lg shadow-lg hover:bg-[#3e455d]  transition transform hover:scale-105 duration-500">
+          ارسال
+        </button>
+      </form>
     </div>
+
+    <div className="flex flex-col gap-6 justify-center">
+      <h3 className="text-3xl font-bold text-[#070B1D] dark:text-white">معلومات للتواصل</h3>
+      <p className="text-gray-400">نحن سعداء بسماعك , اترك لنا رساله او تواصل عبر الارقام </p>
+      
+      <p className="font-semibold text-[#070B1D] dark:text-white"><FaMapPin className=' text-[#11BAF0]' /> <span className='text-2xl text-[#11BAF0]'>العنوان :</span>  123  القاهرة الجديدة , مصر  </p>
+      <p className="font-semibold text-[#070B1D] dark:text-white"><FaPhoneSquareAlt className=' text-[#11BAF0]' /> <span className='text-2xl text-[#11BAF0]'>الهاتف :</span> +2 116514562918</p>
+      {/* اواي اخلي السطر دا انجليزي  */}
+      <p className="font-semibold  text-[#070B1D] dark:text-white"  lang="en"><MdMarkEmailUnread className=' text-[#11BAF0]' /> <span className='text-2xl text-[#11BAF0]'>البريد الالكتروني :</span><span>info@tarekak.com</span>
+</p>
+      <div className="flex items-center gap-4 mt-4">
+        <a href="https://www.facebook.com/" className='text-5xl dark:text-[#a1a7be] text-[#070B1D]'><IoLogoFacebook /></a> 
+        <a href="https://www.linkedin.com/" className='text-4xl dark:text-[#a1a7be] text-[#070B1D]'><BsLinkedin /></a> 
+        <a href="https://www.instagram.com/" className='text-5xl dark:text-[#a1a7be] text-[#070B1D]'><AiFillInstagram /></a> 
+        <a href="https://x.com/" className='text-4xl dark:text-[#a1a7be] text-[#070B1D]'><FaSquareXTwitter /></a>
+      </div>
+
+
+    </div>
+
+  </div>
+</div>
+
+
+
+    </>
   )
 }
 
-export default ContactUs
+export default Contact
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,47 +1,43 @@
-import React from "react";
-import { home } from "../assets/index.js";
-import { Link } from "react-router-dom";
-import Aksam from "./aksam";
-import ContactUs from "./ContactUs";
-import ScrollToTop from "./ScrollToTop";
-const Home = () => {
-  
-  return (
-    <>
-    <ScrollToTop />
-    <div className="container mx-auto md:w-[1200px] dark:bg-slate-950 dark:text-white text-black ">
-      <div className="container mx-auto min-h-screen mt-5">
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10 ">
-          <div className="HomeText md:text-right text-center gap-y-2">
-            <h1 className="font-dinar text-6xl  md:text-7xl">
-              معنا تتحول أحلامك
-            </h1>
-            <h1 className="font-dinar text-5xl md:text-6xl text-[#11baf0] py-5">
-              الى قصة نجاح!
-            </h1>
-            <p className="font-dinar text-lg md:text-xl py-4">
-              طريقك، منصّة تعليمية بتساعدك تتعلم أهم المهارات المطلوبة لسوق
-              العمل زي البرمجة، الجرافيك ديزاين، السوفت سكيلز وغيرهم. <br />{" "}
-              <span className="text-[#11baf0]">
-                معانا، التعلّم أبسط والطريق أوضح.
-              </span>{" "}
-            </p>
-            <div className=" btns grid grid-cols-1 md:grid-cols-2 gap-3 mt-5 md:w-90 w-[90%] mx-auto md:mx-0">
-              <Link to="/courses">
-                <button className="bg-[#11baf0] text-white px-5 py-3 rounded-xl hover:shadow-[0_0_50px_#11b8f0a2] transition-all duration-500">
-                  تصفح الكورسات
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="HomeImage flex justify-center drop-shadow-[0_0_100px_#11b8f0]">
-            <img src={home} alt="" className=" " />
-          </div>
-        </div>
-      </div>
-    </div>
-    </>
-  );
-};
+import React from 'react'
+import { home } from '../assets/index.js'
+import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import ScrollToTop from './ScrollToTop'
 
-export default Home;
+
+const Home = () => {
+    useEffect(() => {
+  AOS.init({
+    duration: 1000, // animation duration in ms
+    once: true,     // whether animation should happen only once
+  });
+}, []);
+  return (
+    <div className='container mx-auto md:w-[1200px] dark:bg-slate-950 dark:text-white text-black '>
+            <ScrollToTop />
+
+        <div className='container mx-auto h-screen mt-5 md:mt-14 '>
+            <div className='grid grid-cols-1 md:grid-cols-2 items-center gap-10'>
+                <div className='HomeText md:text-right text-center gap-y-2'  data-aos="fade-left">
+                    <h1 className='font-dinar text-6xl  md:text-7xl'>معنا تتحول أحلامك</h1>
+                    <h1 className='font-dinar text-5xl md:text-6xl text-[#11baf0] py-5'>الى قصة نجاح!</h1>
+                    <p className='font-dinar text-lg md:text-xl py-4'>طريقك، منصّة تعليمية بتساعدك تتعلم أهم المهارات المطلوبة لسوق العمل زي البرمجة، الجرافيك ديزاين، السوفت سكيلز وغيرهم. <br />   <span className='text-[#11baf0]'>معانا، التعلّم أبسط والطريق أوضح.</span> </p>
+                    <div className=' btns grid grid-cols-1 md:grid-cols-2 gap-3 mt-5 md:w-90 w-[90%] mx-auto md:mx-0'>
+                    <Link to="/courses"><button className='bg-[#11baf0] text-white px-5 py-3 rounded-xl hover:shadow-[0_0_50px_#11b8f0a2] transition-all duration-500'>
+                        اكتشف الآن
+                    </button></Link>
+                    </div>
+                </div>
+                <div className='HomeImage flex justify-center drop-shadow-[0_0_100px_#11b8f0]'  data-aos="fade-right">
+                    <img src={home} alt="" className=""/>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
